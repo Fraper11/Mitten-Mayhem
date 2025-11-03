ff39f094bf3e110ef88040fc17bf62965e465262
Fraper11 <francesco.perillo.00@gmail.com>
2025-11-03 17:22:43 +0100

    Cursor Movement
    Adjusted the Hierarchy of the project's folders.
    Added Cursor Confining

---
+++ Scripts/CursorMovement.gd
@@ -0,0 +1,18 @@
+extends Node
+var  mouseConfine: bool = true
+
+func _ready() -> void:
+	ToggleMouseConfine()
+
+func _process(delta: float) -> void:
+	if (Input.is_action_just_pressed("mouse_confine")):
+		mouseConfine = !mouseConfine
+		ToggleMouseConfine()
+
+func ToggleMouseConfine() -> void :
+	if (mouseConfine):
+		Input.set_mouse_mode (Input.MOUSE_MODE_CONFINED)
+		print("Mouse Confined")
+	else :
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		print("Mouse Freed")

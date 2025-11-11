72f58cd5889c29b7f627eca61c77a0e3c6f04ea8
a25yuthe-007 <a25yuthe@gmail.com>
2025-11-11 14:18:15 +0100

    timecontrol

---
+++ Scripts/timecontrol.gd
@@ -0,0 +1,31 @@
+extends Node2D
+@export var ActionperTurn = 2
+var ActionNow = 0
+var PassedDay = 0
+
+func one_round():
+		if$EMclpointer01.visible == true:
+			$EMclpointer01.visible = false
+			$Mclpointer01.visible = true	
+		elif$Mclpointer01.visible == true:
+			$Mclpointer01.visible = false
+			$Eclpointer01.visible =true
+		elif$Eclpointer01.visible ==true:
+			$Eclpointer01.visible =false
+			$Nclpoint01.visible = true
+		else:
+			$Nclpoint01.visible = false
+			$EMclpointer01.visible = true
+			PassedDay=PassedDay+1
+
+func skip():
+	ActionNow=0
+	one_round()
+
+func action():
+	ActionNow=ActionNow+1
+	if ActionNow==ActionperTurn:
+		skip()
+			
+func _ready():
+	pass

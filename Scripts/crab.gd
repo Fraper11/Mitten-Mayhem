236d3344343ba17a2f093e809505f326a6f4a50d
Pixelyzer <pixelyzer@gmail.com>
2025-12-15 03:04:58 +0100

    Crab movement

--- Scripts/crab.gd
+++ Scripts/crab.gd
@@ -3,55 +3,65 @@ extends Node2D
 @onready var animation_player : AnimationPlayer = $AnimationPlayer
 @onready var sound = $grab
 
+@onready var ray_rw = $RayRightWater
+@onready var ray_lw = $RayLeftWater
+@onready var ray_rl = $RayRightLand
+@onready var ray_ll = $RayLeftLand
+
+const h_speed = 30
+var h_direction = 1
+const v_speed = 20
+var v_direction = 1
+
+var nums = [-1,1]
+var h_dir_timer := Timer.new()
+var v_dir_timer := Timer.new()
+
 var sound_stream : AudioStreamPlayer2D = AudioStreamPlayer2D.new()
 
 var marker_node : Marker2D = null 
-enum states {IDLER,GOINGRIGHT,GOINGLEFT,IDLEL,COLLECTED,DIE}
-var current_state = states.IDLER
+enum states {IDLE,COLLECTED,DIE}
+var current_state = states.IDLE
+
+var on_land = false
 
 func _ready() -> void:
+	add_child(h_dir_timer)
+	h_dir_timer.timeout.connect(_on_h_timer_timeout)
+	h_dir_timer.one_shot = false
+	h_dir_timer.start(randi_range(2,10))
+	
+	add_child(v_dir_timer)
+	v_dir_timer.timeout.connect(_on_v_timer_timeout)
+	h_dir_timer.one_shot = false
+	v_dir_timer.wait_time = 0.25
+	v_dir_timer.start()
+	
 	if(self != null && animation_player != null):
 		run_states()
 		animation_player.speed_scale = 2.0
+	animation_player.play("Idle")
+
+
+func _on_h_timer_timeout() -> void:
+	h_direction = nums[randi() % nums.size()] #Random horizontal direction
+
+
+func _on_v_timer_timeout() -> void:
+	v_direction = -v_direction
+
+
+func _process(delta):
+	# Collisions with land and water
+	if ray_rl.is_colliding() or ray_rw.is_colliding():
+		h_direction = -1
+		
+	elif ray_ll.is_colliding() or ray_lw.is_colliding():
+		h_direction = 1
 	
-func run_states():
-	if(self != null && animation_player != null):
-		match current_state:
-			states.IDLER:
-				
-				animation_player.play("Idle")
-				await animation_player.animation_finished
-				current_state = states.GOINGRIGHT
-				run_states()
-				print(current_state)
-			
-			states.GOINGRIGHT:
-				animation_player.play("crab_moving")
-				await animation_player.animation_finished
-				current_state = states.IDLEL
-				run_states()
-				
-			states.IDLEL:
-				animation_player.play("Idle")
-				await animation_player.animation_finished
-				current_state = states.GOINGLEFT
-				run_states()
-				
-			
-			states.GOINGLEFT:
-				animation_player.play_backwards("crab_moving")
-				await animation_player.animation_finished
-				current_state = states.IDLER
-				run_states()
-				
-			states.COLLECTED:
-				CursorMovement.stop_mouse()
-				current_state = states.DIE
-				run_states()
-			
-			states.DIE:
-				die()
-			
+	# Movement
+	position.x += h_direction * h_speed * delta
+	position.y += v_direction * v_speed * delta
 
 
 func die ():
@@ -71,3 +81,15 @@ func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) 
 		run_states()
 		if (!PlayerData.unlocked_pages.has("PAGE_001")):
 			PlayerData.unlock_page("PAGE_001")
+
+
+func run_states():
+	if(self != null && animation_player != null):
+		match current_state:
+			states.COLLECTED:
+				CursorMovement.stop_mouse()
+				current_state = states.DIE
+				run_states()
+			
+			states.DIE:
+				die()
